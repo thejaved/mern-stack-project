297f36eda86c48bc76fc301992f78ba9761a803d
javed khan <javed.khan@extwebtech.in>
2022-09-24 23:51:21 +0530

    password hashing integrated

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcryptjs.hash(this.password, 12);
+  }
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
